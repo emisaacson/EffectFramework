6c6bf5793fcb1895ab1cc285bbc762d4663c392e
Elliot Isaacson <elliot@emikek.com>
2015-07-01 02:44:59 -0400

    Reformatting for easier updating

--- EffectFramework.Core/Strings.cs
+++ EffectFramework.Core/Strings.cs
@@ -7,8 +7,11 @@ namespace EffectFramework.Core
 {
     public class Strings
     {
-        public const string Field_Is_Required = "The field {0}: {1} is required.";
-        public const string Range_Is_Required = "The field {0}: {1} must be between {2} and {3}.";
-        public const string Field_Does_Not_Validate = "The field {0}: {1} does not validate.";
+        public const string 
+              Field_Is_Required = "The field {0}: {1} is required."
+            , Range_Is_Required = "The field {0}: {1} must be between {2} and {3}."
+            , Field_Does_Not_Validate = "The field {0}: {1} does not validate."
+            
+            ;
     }
 }
